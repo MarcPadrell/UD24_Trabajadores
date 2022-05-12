82b697fa07dc5bec95fc17cdae6f9913bbbc4c16
MarcPadrell <marc.padrell@hotmail.com>
2022-05-12 13:25:24 +0200

    Updates done!

--- UD24_MavenSpring_Empleados/src/main/resources/data.sql
+++ UD24_MavenSpring_Empleados/src/main/resources/data.sql
@@ -3,14 +3,14 @@ DROP table IF EXISTS trabajador;
 
 
 create table trabajador(
-	id int auto_increment,
+	id int primary key auto_increment,
 	nombre varchar(250),
 	trabajo varchar(250),
 	salario double default null
 );
 
-/*insert into trabajador (nombre, trabajo, salario)values('Jose','IT',1850.56);
+insert into trabajador (nombre, trabajo, salario)values('Jose','IT',1850.56);
 insert into trabajador (nombre, trabajo, salario)values('Juan','RRHH',1300.30);
-insert into trabajador (nombre, trabajo)values('Pedro','Marketing');
-insert into trabajador (nombre, trabajo)values('Jordi','FullStack');
-insert into trabajador (nombre, trabajo)values('Jonatan','Consultor');*/
+insert into trabajador (nombre, trabajo, salario)values('Pedro','Marketing',1400.80);
+insert into trabajador (nombre, trabajo, salario)values('Jordi','FullStack',2500.95);
+insert into trabajador (nombre, trabajo, salario)values('Jonatan','Consultor',3000.40);
